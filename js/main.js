a6daf8317a110432269c262e2770dbdd1cf6630a
CerealKiller97 <bogdanovicstefan1997@gmail.com>
2018-02-09 00:09:38 +0100

    robots and sitemap and animations

--- js/main.js
+++ js/main.js
@@ -2,10 +2,18 @@ particlesJS.load('particles-js', 'assets/particlesjs-config.json')
 
 const toggle = document.querySelector('#toggle')
 const aside = document.querySelector('aside')
-
+const loader = document.querySelector('#loader')
 
 let showSidebar = () => {
   aside.classList.toggle('active')
 }
+
+setTimeout(() => {
+  loader.classList.add('opacity')
+}, 3000)
+
+setTimeout(() => {
+  loader.classList.add('done')
+}, 4000)
 
 toggle.addEventListener('click', showSidebar)
